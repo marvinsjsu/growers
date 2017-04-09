f73d243d6679a21d517365853d5901fd25c7f13b
Marvin Mante <mmante@sugarcrm.com>
2017-04-09 14:16:18 -0700

    added new homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,7 +84,8 @@
 
     <!-- Scripts -->
     <script src="/js/app.js"></script>
-<!-- include summernote css/js-->
+
+    <!-- include summernote css/js-->
 <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.css" rel="stylesheet">
 <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.js"></script>
 <script>
